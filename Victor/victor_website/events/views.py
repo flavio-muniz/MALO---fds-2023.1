5d0917e8fd27a62b3cb1b103cc9b8bb49f4e5032
VictorHTenorio <victorhorten7@gmail.com>
2023-04-01 23:57:37 -0300

    calendário

--- Victor/victor_website/events/views.py
+++ Victor/victor_website/events/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html',{})
+def home(request,year,month):
+    name = "Victor"
+    month = month.title() #convertendo para minúsculo
+    #convertendo mes de string para int
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    #criando calendário
+    cal = HTMLCalendar().formatmonth(year,month_number)
+
+    #pegando o ano atual
+    now = datetime.now()
+    current_year = now.year
+
+    #pegando o tempo atual
+    time= now.strftime('%I:%M:%S %p')
+    return render(request, 'home.html',{
+        "first_name" : name,
+        "year" : year,
+        "month" : month,
+        "month_number" : month_number,
+        "cal": cal,
+        "current_year" : current_year,
+        'time' : time,
+    })
