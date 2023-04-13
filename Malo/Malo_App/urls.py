d33e2f69dbe4db979d05dde7bbe779f890939a73
VictorHTenorio <victorhorten7@gmail.com>
2023-04-12 21:49:50 -0300

    malo start

---
+++ Malo/Malo_App/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Path Convertes
+    # int: numbers
+    # str: strings
+    # path: todas urls
+    # slug: hyphen-and_underscores_stuff
+    # UUID: universally unique identifier
+    # path('', views.home, name="home"),
+    path('<int:year>/<str:month>/', views.calendario, name="calendario"),
+    path('', views.calendario, name="calendario"),
+    path('login/', views.login_user, name="login"),
+]
