dce8ca48044294112787db7d43786a0b7197453a
Cenafowzin <digodubeux@gmail.com>
2023-04-14 20:58:47 -0300

    adicionar/remover/editar pratos e ingredientes

--- Malo/Malo_App/urls.py
+++ Malo/Malo_App/urls.py
@@ -9,9 +9,11 @@ urlpatterns = [
     path('ingredient-list/', views.All_ingredient, name='ingredient_list'),
     path('add-ingredientes/', views.Add_ingredient, name='add_ingredient'),
     path('delete-ingredient/<ingredient_id>', views.Delete_ingredient, name='delete_ingredient'),
+    path('edit-ingredient/<ingredient_id>', views.Edit_ingredient, name='edit_ingredient'),
     path('menu/', views.Menu, name='menu'),
     path('add-dishes/', views.Add_dish, name='add_dish'),
     path('edit-dish/<dish_id>', views.Edit_dish, name='edit_dish'),
+    path('delete-dish/<dish_id>', views.Delete_dish, name='delete_dish'),
     path('mesa/', views.all_Mesa, name='mesa'),
     path('add_mesa', views.add_mesa, name='add_mesa'),
 ]
