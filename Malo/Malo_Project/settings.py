b2c6607838c1cd47fe0d2815013f66fb67ef1a8f
VictorHTenorio <victorhorten7@gmail.com>
2023-04-17 12:39:50 -0300

    aws4

--- Malo/Malo_Project/settings.py
+++ Malo/Malo_Project/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Malo_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'Utils.db.backends.sqlite3',
+        'ENGINE': 'utils.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
