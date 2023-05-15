cf86a2b6ac8cf77f99c608cc081ee444453e11ab
pdrvsk <nouoneq@riseup.net>
2023-05-14 23:27:04 -0300

    Adicionando mais testes

--- Malo/Malo_App/tests.py
+++ Malo/Malo_App/tests.py
@@ -1,7 +1,9 @@
 from django import test
+from django.forms import Select
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 
 
@@ -16,7 +18,7 @@ class TestHome(LiveServerTestCase):
     def testTitle(self):
         # Checa se o titulo está correto
         assert 'MALO' in self.driver.title
-
+    
     def test1CriarLoginGerente(self):
         self.setUp()
 
@@ -42,7 +44,7 @@ class TestHome(LiveServerTestCase):
         self.driver.find_element(By.CLASS_NAME, 'password2').send_keys('Senhamassa')
         criarLogin = self.driver.find_element(By.CLASS_NAME, 'submit').submit()
         print(criarLogin, 'teste')
-
+    
 
 
     def Login(self):
@@ -64,7 +66,110 @@ class TestHome(LiveServerTestCase):
         else:
             return 'User not logged in'
         
-       
+    def testOpenMesa(self):
+        self.Login()
+        omesa = self.driver.find_element(By.CLASS_NAME, 'mesa')
+        assert omesa.get_attribute('href') == "http://127.0.0.1:8000/mesa/"
+
+
+    def testAdd1Mesa(self):
+        self.Login()
+        self.driver.find_element(By.CLASS_NAME, 'mesa').click()
+        self.driver.find_element(By.CLASS_NAME, 'add_mesa').click()
+
+
+    def testAddMultMesas(self):
+        self.Login()
+        self.driver.find_element(By.CLASS_NAME, 'mesa').click()
+        self.driver.find_element(By.ID, 'add_qtd_mesas').send_keys('4')
+        self.driver.find_element(By.CLASS_NAME, 'submit_add_mesas').click()
+
+    def testRem1Mesa(self):
+        self.Login()
+        self.driver.find_element(By.CLASS_NAME, 'mesa').click()
+        self.driver.find_element(By.ID, 'rem_mesa').click()
+
+
+    def testRemMultMesas(self):
+        self.Login()
+        self.driver.find_element(By.CLASS_NAME, 'mesa').click()
+        self.driver.find_element(By.ID, 'rem_qtd_mesas').send_keys('2')
+        self.driver.find_element(By.CLASS_NAME, 'submit_rem_mesas').click()
+
+    def testAddCategory1(self):
+        self.Login()
+        self.driver.get('http://127.0.0.1:8000/home/')
+        self.driver.find_element(By.CLASS_NAME, 'menu_category').click()
+        self.driver.find_element(By.CLASS_NAME, 'add_category').click()
+        self.driver.find_element(By.CLASS_NAME, 'categoria').send_keys('Bebidass')
+        self.driver.find_element(By.CLASS_NAME, 'enviar').click()
+
+
+    def testAddCategory2(self):
+        self.Login()
+        self.driver.get('http://127.0.0.1:8000/home/')
+        self.driver.find_element(By.CLASS_NAME, 'menu_category').click()
+        self.driver.find_element(By.CLASS_NAME, 'add_category').click()
+        self.driver.find_element(By.CLASS_NAME, 'categoria').send_keys('Comidas')
+        self.driver.find_element(By.CLASS_NAME, 'enviar').click()
+
+    def testAddCategoryTemp(self):
+        self.Login()
+        self.driver.get('http://127.0.0.1:8000/home/')
+        self.driver.find_element(By.CLASS_NAME, 'menu_category').click()
+        self.driver.find_element(By.CLASS_NAME, 'add_category').click()
+        self.driver.find_element(By.CLASS_NAME, 'categoria').send_keys('Categoria para remover')
+        self.driver.find_element(By.CLASS_NAME, 'enviar').click()
+  
+        
+    def testRemCategoryTemp(self):
+        self.Login()
+        self.driver.get('http://127.0.0.1:8000/home/')
+        self.driver.find_element(By.CLASS_NAME, 'menu_category').click()
+        self.driver.find_element(By.NAME, 'rem_Categoria para remover').click()
+
+    def testAddDish1(self):
+        self.Login()
+        self.driver.get('http://127.0.0.1:8000/add-dishes/')
+        elemento_select = self.driver.find_element(By.NAME, "category")
+
+        dropdown = Select(elemento_select)
+        elemento_select.click()
+        dropdown.select_by_visible_text("Bebidas")
+
+        self.driver.find_element(By.NAME, 'name').send_keys('Coca-Cola')
+        self.driver.find_element(By.NAME, 'price').send_keys('8')
+        self.driver.find_element(By.NAME, 'description').send_keys('Coca-Cola ks')
+
+        self.driver.find_element(By.CLASS_NAME, 'submit').click()
+
+    def testAddDish2(self):
+        self.Login()
+        self.driver.get('http://127.0.0.1:8000/add-dishes/')
+        elemento_select = self.driver.find_element(By.NAME, "category")
+
+        dropdown = Select(elemento_select)
+        elemento_select.click()
+        dropdown.select_by_visible_text("Comidas")
+
+        self.driver.find_element(By.NAME, 'name').send_keys('Suco de laranja')
+        self.driver.find_element(By.NAME, 'price').send_keys('10')
+        self.driver.find_element(By.NAME, 'description').send_keys('Suco de laranja 300ml')
+
+        self.driver.find_element(By.CLASS_NAME, 'submit').click()
+
+   
+    
+    def testEditCategory(self):
+        self.Login()
+        self.driver.get('http://127.0.0.1:8000/home/')
+        self.driver.find_element(By.CLASS_NAME, 'menu_category').click()
+        self.driver.find_element(By.CLASS_NAME, 'edit_Bebidass').click()
+        self.driver.find_element(By.CLASS_NAME, 'categoria').clear()
+        self.driver.find_element(By.CLASS_NAME, 'categoria').send_keys('Bebidas')
+
+        self.driver.find_element(By.CLASS_NAME, 'submit').click()
+
 
     def testOpenMenu(self):
         self.Login()
@@ -82,28 +187,26 @@ class TestHome(LiveServerTestCase):
 
     def testOpenMenuList(self):
         self.Login()
-        self.driver.get('http://127.0.0.1:8000/menu-category/')
+        self.driver.get('http://127.0.0.1:8000/home/')
+        self.driver.find_element(By.CLASS_NAME, 'menu_category').click()
         menu = self.driver.find_element(By.CLASS_NAME, 'menu')
         assert menu.get_attribute('href') == "http://127.0.0.1:8000/menu/"
         
     def testOpenAddCategory(self):
         self.Login()
-        self.driver.get('http://127.0.0.1:8000/menu-category/')
+        self.driver.get('http://127.0.0.1:8000/home/')
+        self.driver.find_element(By.CLASS_NAME, 'menu_category').click()
         oaddcat = self.driver.find_element(By.CLASS_NAME, 'add_category')
         assert oaddcat.get_attribute('href') == "http://127.0.0.1:8000/add-category/"
 
-    def testAddCategory(self):
-        self.Login()
-        self.driver.get('http://127.0.0.1:8000/add-category/')
-        self.driver.find_element(By.CLASS_NAME, 'categoria').send_keys('categoria teste')
-        self.driver.find_element(By.CLASS_NAME, 'enviar').click()
+    
 
     def testLogout(self):
         self.Login()
         logout = self.driver.find_element(By.CLASS_NAME, 'logout')
         assert logout.get_attribute('href') == "http://127.0.0.1:8000/logout/"
         print(self.testLoginGerente())
-    
+
     def tearDown(self):
         self.driver.quit()
 
