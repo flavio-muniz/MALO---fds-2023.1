8e5f2a148276859e72202ae9bf52282edf1388f2
VictorHTenorio <victorhorten7@gmail.com>
2023-05-31 20:42:55 -0300

    test 10

--- Malo_App/tests.py
+++ Malo_App/tests.py
@@ -1,25 +1,18 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+browser = webdriver.Chrome(options=chrome_options)
+
+
+# Create your tests here.
 
 class TestHome(LiveServerTestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        chrome_options = Options()
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-gpu")
-        cls.browser = webdriver.Chrome(options=chrome_options)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.browser.quit()
-        super().tearDownClass()
-
     def test_title(self):
-        self.browser.get('http://127.0.0.1:8000/')
-       
+        browser.get('http://127.0.0.1:8000/')
 
  
