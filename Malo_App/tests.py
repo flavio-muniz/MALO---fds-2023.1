83dcc881b5f442454d551885802476462349ec1d
VictorHTenorio <victorhorten7@gmail.com>
2023-05-31 23:09:06 -0300

    menu_category btn btn-secondary btn-xl mb-3

--- Malo_App/tests.py
+++ Malo_App/tests.py
@@ -81,7 +81,7 @@ class TestHome(LiveServerTestCase):
     def test3AddCategory1(self):
         self.Login()
         self.driver.get('http://127.0.0.1:8000/home/')
-        self.driver.find_element(By.CLASS_NAME, 'menu_category').click()
+        self.driver.find_element(By.CLASS_NAME, 'menu_category btn btn-secondary btn-xl mb-3').click()
         self.driver.find_element(By.CLASS_NAME, 'add_category').click()
         self.driver.find_element(By.CLASS_NAME, 'categoria').send_keys('Bebidas')
         self.driver.find_element(By.CLASS_NAME, 'enviar').click()
